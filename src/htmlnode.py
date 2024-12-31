967df66eafe7496f01d7b946be30872ba35fe9c4
Lewis Benson <tm.lewisbenson@gmail.com>
2024-12-30 22:22:10 -0500

    add html and nodes

---
+++ src/htmlnode.py
@@ -0,0 +1,10 @@
+class HTMLNode:
+  def __init__(self, tag, value, children, props):
+    self.tag = None
+    self.value = None
+    self.children = None
+    self.props = None
+  def to_html(self):
+    pass
+  def props_to_html(self):
+    pass
